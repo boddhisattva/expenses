2098803bbfa315cab43049dd0f38498fd3514aea
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-18 06:30:57 +0530

    uses double quotes and removes unnecessary extra line

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,45 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "StaticPages", type: :request do
   describe "#home" do
     it "should have the title Home" do
-      visit '/static_pages/home'
+      visit "/static_pages/home"
 
-      expect(page).to have_title('Expenses | Home')
+      expect(page).to have_title("Expenses | Home")
     end
 
     it "should have basic info about the app" do
-      visit '/static_pages/home'
+      visit "/static_pages/home"
 
-      expect(page).to have_content('Tracking expenses made easy')
+      expect(page).to have_content("Tracking expenses made easy")
     end
   end
 
   describe "#about" do
     it "should have the title About" do
-      visit '/static_pages/about'
+      visit "/static_pages/about"
 
-      expect(page).to have_title('Expenses | About')
+      expect(page).to have_title("Expenses | About")
     end
 
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit "/static_pages/about"
 
-      expect(page).to have_content('About')
+      expect(page).to have_content("About")
     end
   end
 
   describe "#help" do
     it "should have the title Help" do
-      visit '/static_pages/help'
+      visit "/static_pages/help"
 
-      expect(page).to have_title('Expenses | Help')
+      expect(page).to have_title("Expenses | Help")
     end
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit "/static_pages/help"
 
-      expect(page).to have_content('Help')
+      expect(page).to have_content("Help")
     end
   end
-
 end
