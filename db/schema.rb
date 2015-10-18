80f59d028e21fd7bcfbb69fe852166ec03541792
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-18 14:55:05 +0530

    adds a unique index against user email to prevent duplicate records dquote> dquote>   - In a traffic heavy website the rails validation to check for uniquness isn't enough dquote>   - This check against a unique index name for an email provides a DB level constraint to prevent two users from having the same email when registering

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219120902) do
+ActiveRecord::Schema.define(version: 20151018064722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "expenses", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.float    "cost",       null: false
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "expenses", ["user_id"], name: "index_expenses_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "name"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+  add_foreign_key "expenses", "users"
 end
