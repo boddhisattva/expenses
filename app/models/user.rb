3480b9ae3efd8ad35ce0acd53fbedc6bf0d63508
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-18 13:44:33 +0530

    adds validations to check for presence of name and limit its length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  validates :email, presence:true
+  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
 
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  before do
+    @user = FactoryGirl.build(:user)
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
   context "given a new User instance" do
     it "should validate the presence of email" do
       expect(User.new(:email => "")).not_to be_valid
     end
   end
+
 end
