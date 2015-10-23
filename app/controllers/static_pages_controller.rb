aab014c4c39d1ae98dc220b73ff225d5d659aa27
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-23 19:40:04 +0530

    removes extra whitespace

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @user_expenses  = current_user.expenses_feed
+      @user_expenses = current_user.expenses_feed
     end
   end
 
