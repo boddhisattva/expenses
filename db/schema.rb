f899056a439092cdf9f30004db49dcd33bc78892
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-24 11:12:17 +0530

    Adds a DB index to render the app homepage faster
    User expenses are displayed on app homepage. As the number of expenses
    grow in number this multiple key index(based on the user_id and the
    created_at) would help fetch that data from the table faster. For more
    details, one could consider referring the link -
    http://stackoverflow.com/questions/4445507/rails-created-at-when-user-fo
    r-ordering-should-you-add-an-index-to-the-table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021032750) do
+ActiveRecord::Schema.define(version: 20151024001015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151021032750) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "expenses", ["user_id", "created_at"], name: "index_expenses_on_user_id_and_created_at", using: :btree
   add_index "expenses", ["user_id"], name: "index_expenses_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
