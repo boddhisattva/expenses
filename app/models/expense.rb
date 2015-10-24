861e01a6db127a97f0aa79db95af133d2076aa9c
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-24 11:27:10 +0530

    Adds a validation to ensure that the cost is a number

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ActiveRecord::Base
   validates :user, presence: true
-  validates :cost, presence: true
+  validates :cost, presence: true, numericality: true
   validates :name, presence: true
   validates :date, presence: true
 
