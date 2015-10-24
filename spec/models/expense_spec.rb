7f4d0f29bcd316e18c1c4bcfbfcb4dac754554f9
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-24 12:34:49 +0530

    Adds specs wrt use cases when an expense is invalid

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -22,4 +22,29 @@ RSpec.describe Expense, type: :model do
     end
   end
 
+  describe "when expense cost is not present" do
+    before { @expense.cost = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when expense name is not present" do
+    before { @expense.name = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when expense date is not present" do
+    before { @expense.date = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when user_id is not present" do
+    before { @expense.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when cost is not a number" do
+    before { @expense.cost = "12a3" }
+    it { should_not be_valid }
+  end
+
 end
