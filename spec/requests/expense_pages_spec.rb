6fcdfd6df24ba0a5fe21616e2cde56ad15d9aaeb
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-25 07:10:31 +0530

    Adds expense creation related specs
    As of this commit expense creation in the app happens based on two
    scenarios, namely -
    1. When a user has no expenses at all
    2. When a user has atleast one expense
    
    If you observe closely, you may find that for scenario 2 there are no
    specs for the valid and invalid information checks when going about
    creating an expense. This is basically because scenario 2 goes to the
    same page to create a new expense as is in the case of scenario 1. Thus
    in scenario 2 it is sufficient if we test that a user goes to same
    page(as the one used in case of scenario 1) where one can go about
    creating a new expense.

---
+++ spec/requests/expense_pages_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+describe "Expense pages", type: :request do
+
+  subject { page }
+
+  let(:user) { FactoryGirl.create(:user) }
+  before { log_in user }
+
+  describe "expense creation" do
+    context "user is creating their first expense" do
+      before do
+        visit root_path
+        click_link "create a new expense"
+      end
+
+      describe "with invalid information" do
+
+        it "should not create an expense" do
+          expect { click_button "Create expense" }.not_to change(Expense, :count)
+        end
+
+        describe "error messages" do
+          before { click_button "Create expense" }
+          it { should have_content('error') }
+        end
+      end
+
+      describe "with valid information" do
+
+        before do
+          fill_in 'expense_name', with: "Sunglasses"
+          fill_in 'expense_cost', with: "1000"
+          fill_in 'expense_date', with: Time.zone.today
+        end
+        it "should create a expense" do
+          expect { click_button "Create expense" }.to change(Expense, :count).by(1)
+        end
+      end
+    end
+
+    context "user has atleast one expense created" do
+      before do
+        FactoryGirl.create(:expense, user: user)
+        visit root_path
+        click_link "Create a new expense"
+      end
+
+      it { expect(page).to have_selector(:link_or_button, 'Create expense') }
+    end
+  end
+end
