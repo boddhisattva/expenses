346beb5a807362b4fa289736d913c506d48100ae
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-25 18:14:29 +0530

    Adds expenses updation related specs
    There are a couple of things to note here.
    
    Before we could delve into talking about the code changes, I’d like to
    first add that only when an expense is created that we go about
    actually seeing the ‘Edit’ link wrt user home page. Let’s call this as
    point 1.
    
    Now coming to the code related changes -
    1. We’ve made use of let with a bang(!) because we can’t afford the
    expense to be lazy loaded. An expense has to be created before hand so
    that we could make use of the ‘Edit’ link as mentioned in point 1.
    
    2. Since we’re actually creating an expense from the backend we need to
    actually refresh the user home page. Hence we’ve added the code related
    to ‘visit root_path’. Only with this line of code we’ll see the ‘Edit’
    link.
    
    3. When updating an expense we’re basically considering two scenarios -
    a. With invalid expense information
    b. With valid expense information

--- spec/requests/expense_pages_spec.rb
+++ spec/requests/expense_pages_spec.rb
@@ -48,4 +48,45 @@ describe "Expense pages", type: :request do
       it { expect(page).to have_selector(:link_or_button, 'Create expense') }
     end
   end
+
+  describe "expense updation" do
+    let!(:expense) { FactoryGirl.create(:expense, user: user) }
+
+    before do
+      visit root_path
+      click_link "Edit"
+    end
+
+    describe "with invalid information" do
+      describe "error messages" do
+        context "name is empty" do
+          before do
+            fill_in 'expense_name', with: ""
+            click_button "Update expense"
+          end
+
+          it { should have_content("The form contains 1 error.") }
+          it { should have_content("Name can\'t be blank") }
+        end
+      end
+    end
+
+    describe "with valid information" do
+        let(:new_expense_cost)  { 15 }
+        let(:new_expense_name)  { "Coffee" }
+        before do
+          fill_in 'expense_cost', with: new_expense_cost
+          fill_in 'expense_name', with: new_expense_name
+          click_button "Update expense"
+        end
+
+        it { should have_content("Expense updated") }
+        it "should update the expense with the correct cost" do
+          expect(expense.reload.cost).to  eq new_expense_cost
+        end
+        it "should update the expense with the correct name" do
+          expect(expense.reload.name).to  eq new_expense_name
+        end
+    end
+  end
 end
