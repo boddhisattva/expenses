7eaf1e4625851e52d2793312996e751cc4468931
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-31 18:32:53 +0530

    Ensures logged in user can't access login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
 
   def new
+    if logged_in?
+      redirect_to root_url, flash: {danger: "You are already logged in"}
+    end
   end
 
   def create
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe SessionsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    context "logged in user tries to access login page" do
+      it "should be redirected to home page" do
+        user = FactoryGirl.create(:user)
+        log_in(user)
+
+        get :new
+
+        expect(response).to redirect_to(root_url)
+      end
+    end
   end
 
 end
