25714aaf9b200481626351b2304b65df34bd7c26
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-31 10:25:14 +0530

    Adds integration specs related to the calculation of total expenses

--- spec/requests/expense_pages_spec.rb
+++ spec/requests/expense_pages_spec.rb
@@ -107,4 +107,58 @@ describe "Expense pages", type: :request do
       expect(page).to have_content("Expense deleted successfully")
     end
   end
+
+  describe "total expenses calculation" do
+    describe "with invalid information" do
+      let!(:expense) { FactoryGirl.create(:expense, user: user) }
+
+      before do
+        visit root_path
+        click_link "Calculate total expenses"
+        fill_in "to_date",   with: Time.zone.tomorrow
+        click_button "Calculate total"
+      end
+
+      it "should have the appropriate error related message", js: true do
+        expect(page).to have_content("The form contains 1 error.")
+      end
+
+      it "should show the appropriate error message", js: true do
+        expect(page).to have_content("From date can\'t be blank")
+      end
+    end
+
+    describe "with valid information" do
+      context "there are incurred expenses with the specififed period" do
+
+        it "should calculate the total expenses accurately", js: true do
+          FactoryGirl.create(:expense, user: user)
+          FactoryGirl.create(:expense, user: user, name: "Petrol", cost: 68.50, date: Time.zone.yesterday)
+          FactoryGirl.create(:expense, user: user, name: "Mug", cost: 50, date: Time.zone.tomorrow)
+
+          visit root_path
+          click_link "Calculate total expenses"
+          fill_in "from_date", with: Time.zone.yesterday
+          fill_in "to_date",   with: Time.zone.tomorrow
+          click_button "Calculate total"
+
+          expect(page).to have_content("128.5")
+        end
+      end
+
+      context "there are no expenses made within the specified period" do
+        it "should display an appropriate message about no expenses incurred", js: true do
+          FactoryGirl.create(:expense, user: user, name: "Petrol", cost: 68.50, date: Time.zone.tomorrow)
+
+          visit root_path
+          click_link "Calculate total expenses"
+          fill_in "from_date", with: Time.zone.now.beginning_of_year
+          fill_in "to_date",   with: Time.zone.today
+          click_button "Calculate total"
+
+          expect(page).to have_content("There were no expenses incurred")
+        end
+      end
+    end
+  end
 end
