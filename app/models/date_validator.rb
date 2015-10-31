00e6ef999032221f41c0257c284b47fa4c0471b2
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-10-31 11:19:26 +0530

    Adds a validation to ensure that "from date" is lesser than "to date"
    To calculate expenses within a given time period, the from date should
    be less than to date.

--- app/models/date_validator.rb
+++ app/models/date_validator.rb
@@ -5,4 +5,15 @@ class DateValidator
 
   validates :from_date, presence: true
   validates :to_date, presence: true
+  validate :from_date_before_to_date, if: :from_date_and_to_date_present?
+
+  def from_date_and_to_date_present?
+    from_date.present? && to_date.present?
+  end
+
+  def from_date_before_to_date
+    if Date.parse(from_date) > Date.parse(to_date)
+      errors.add(:from_date, "cannot be greater than to date")
+    end
+  end
 end
